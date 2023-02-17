dd5b983cacf15047e3d22a4d3de2eaaa80b07c0a
Gui-dev <adrianon10@gmail.com>
2023-02-17 04:10:26 -0300

    feat: add socket.io connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,30 @@
 import https from 'https'
 import fs from 'node:fs'
+import { Server } from 'socket.io'
 
+import { Routes } from './routes.js'
 import { logger } from './logger.js'
+
 
 const PORT = process.env.PORT || 3333
 const localHostSSL = {
   key: fs.readFileSync('./certificates/key.pem'),
   cert: fs.readFileSync('./certificates/cert.pem'),
 }
-
+const routes = new Routes()
 const server = https.createServer(
   localHostSSL,
-  (req, res) => {
-    res.end('Hello World')
-  }
+  routes.handler.bind(routes)
 )
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    credentials: false
+  }
+})
+
+io.on('connection', socket => logger.info(`Someone connected: ${socket.id}`))
 
 const startServer = () => {
   const { address, port } = server.address()
