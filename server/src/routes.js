12972f6110e4d90fcbeffe1f26cd67be5849cc47
Gui-dev <adrianon10@gmail.com>
2023-02-21 03:11:06 -0300

    feat: add implementing the get method

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,19 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import { logger } from './logger.js'
+import { FileHelper } from './file-helper.js'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const defaultDownloadsFolder = resolve(__dirname, '../', 'downloads')
+
 
 export class Routes {
   io
-  constructor () {}
+  constructor (downloasdFolder = defaultDownloadsFolder) {
+    this.downloasdFolder = downloasdFolder
+    this.fileHelper = FileHelper
+  }
 
   setSocketInstance (io) {
     this.io = io
@@ -18,8 +29,9 @@ export class Routes {
   }
 
   async get (request, response) {
-    logger.info('get')
-    response.end()
+    const files = await this.fileHelper.getFilesStatus(this.downloasdFolder)
+    response.writeHead(200)
+    response.end(JSON.stringify(files))
   }
 
   async post (request, response) {
