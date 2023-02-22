3dc0cd78b633202d2d5fb61a28e348858c133088
Gui-dev <adrianon10@gmail.com>
2023-02-22 04:18:34 -0300

    test: it should call emit functions and it is a transform stream

--- server/src/__test__/unit/upload-handler.spec.js
+++ server/src/__test__/unit/upload-handler.spec.js
@@ -1,7 +1,9 @@
-import { describe, expect, jest, it } from '@jest/globals'
+import { beforeEach, describe, expect, jest, it } from '@jest/globals'
 import fs from 'node:fs'
 import { resolve } from 'node:path'
+import { pipeline } from 'node:stream/promises'
 
+import { logger } from './../../logger'
 import { UploadHandler } from './../../upload-handler'
 import { TestUtil } from './../_util/test-util'
 import { Routes } from './../../routes'
@@ -11,6 +13,10 @@ describe('#UploadHandler', () => {
     to: (id) => ioObj,
     emit: (event, message) => {}
   }
+
+  beforeEach(() => {
+    jest.spyOn(logger, 'info').mockImplementation()
+  })
 
   describe('#registerEvents', () => {
     it('should call onFile and onFinish functions on Busboy instance', () => {
@@ -52,6 +58,28 @@ describe('#UploadHandler', () => {
       expect(fs.createWriteStream).toHaveBeenCalledWith(expectedFilename)
       expect(onData.mock.calls.join()).toEqual(chunks.join())
       expect(onTransform.mock.calls.join()).toEqual(chunks.join())
+    })
+  })
+
+  describe('#handleFileBytes', () => {
+    it('should call emit functions and it is a transform stream', async () => {
+      jest.spyOn(ioObj, ioObj.to.name)
+      jest.spyOn(ioObj, ioObj.emit.name)
+      const uploadHanlder = new UploadHandler({ io: ioObj, socketId: '01', downloadsFolder: '/tmp' })
+      const messages = ['hello world']
+      const source = TestUtil.generateReadableStream(messages)
+      const onWrite = jest.fn()
+      const target = TestUtil.generateWritableStream(onWrite)
+      await pipeline(
+        source,
+        uploadHanlder.handleFileBytes('filename.txt'),
+        target
+      )
+
+      expect(ioObj.to).toHaveBeenCalledTimes(messages.length)
+      expect(ioObj.emit).toHaveBeenCalledTimes(messages.length)
+      expect(onWrite).toBeCalledTimes(messages.length)
+      expect(onWrite.mock.calls.join()).toEqual(messages.join())
     })
   })
 })
