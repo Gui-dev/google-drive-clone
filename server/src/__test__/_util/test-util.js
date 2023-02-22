70734c47edc60792d801ff65d5fdec1bb4e9761f
Gui-dev <adrianon10@gmail.com>
2023-02-22 03:25:59 -0300

    test: add helper static method generateWritableStream

--- server/src/__test__/_util/test-util.js
+++ server/src/__test__/_util/test-util.js
@@ -1,14 +1,24 @@
-import { Readable } from 'node:stream'
+import { Readable, Writable } from 'node:stream'
 
 export class TestUtil {
   static generateReadableStream (data) {
     return new Readable ({
       objectMode: true,
-      async read () {
+      read () {
         for (const item of data) {
           this.push(item)
         }
         this.push(null)
+      }
+    })
+  }
+
+  static generateWritableStream (onData) {
+    return new Writable ({
+      objectMode: true,
+      write (chunk, encoding, callback) {
+        onData(chunk)
+        callback(null, chunk)
       }
     })
   }
