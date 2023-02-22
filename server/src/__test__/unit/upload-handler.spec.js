794012447783b3f25ac573abafa04ce2346e1495
Gui-dev <adrianon10@gmail.com>
2023-02-22 03:40:04 -0300

    test: given a stream file it should save it on disk

--- server/src/__test__/unit/upload-handler.spec.js
+++ server/src/__test__/unit/upload-handler.spec.js
@@ -1,4 +1,6 @@
 import { describe, expect, jest, it } from '@jest/globals'
+import fs from 'node:fs'
+import { resolve } from 'node:path'
 
 import { UploadHandler } from './../../upload-handler'
 import { TestUtil } from './../_util/test-util'
@@ -12,7 +14,7 @@ describe('#UploadHandler', () => {
 
   describe('#registerEvents', () => {
     it('should call onFile and onFinish functions on Busboy instance', () => {
-      const uploadHanlder = new UploadHandler({ io: ioObj, socketId: '01' })
+      const uploadHanlder = new UploadHandler({ io: ioObj, socketId: '01', downloadsFolder: '/tmp' })
       jest.spyOn(uploadHanlder, uploadHanlder.onFile.name).mockResolvedValue()
       const headers = { 'content-type': 'multipart/form-data; boundary=' }
       const onFinish = jest.fn()
@@ -23,6 +25,33 @@ describe('#UploadHandler', () => {
 
       expect(uploadHanlder.onFile).toHaveBeenCalled()
       expect(onFinish).toHaveBeenCalled()
+    })
+  })
+
+  describe('#onFile', () => {
+    it('given a stream file it should save it on disk', async () => {
+      const chunks = ['hey', 'dude']
+      const downloadsFolder = '/fake-folder'
+      const uploadHanlder = new UploadHandler({ io: ioObj, socketId: '01', downloadsFolder })
+      const onData = jest.fn()
+      jest.spyOn(fs, fs.createWriteStream.name).mockImplementation(
+        () => TestUtil.generateWritableStream(onData)
+      )
+      const onTransform = jest.fn()
+      jest.spyOn(uploadHanlder, uploadHanlder.handleFileBytes.name).mockImplementation(
+        () => TestUtil.generateTransformStream(onTransform)
+      )
+      const params = {
+        fieldname: 'video',
+        file: TestUtil.generateReadableStream(chunks),
+        filename: 'fake-file.mov'
+      }
+      await uploadHanlder.onFile(...Object.values(params))
+      const expectedFilename = resolve(uploadHanlder.downloadsFolder, params.filename)
+
+      expect(fs.createWriteStream).toHaveBeenCalledWith(expectedFilename)
+      expect(onData.mock.calls.join()).toEqual(chunks.join())
+      expect(onTransform.mock.calls.join()).toEqual(chunks.join())
     })
   })
 })
