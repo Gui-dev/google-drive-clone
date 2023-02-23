1f38d2f67c830d829f91feb67cbb12ee69c2e533
Gui-dev <adrianon10@gmail.com>
2023-02-23 03:11:12 -0300

    feat: add helper method canExecute

--- server/src/upload-handler.js
+++ server/src/upload-handler.js
@@ -5,19 +5,28 @@ import fs from 'node:fs'
 import { logger } from './logger'
 
 export class UploadHandler {
-  constructor ({ io, socketId, downloadsFolder }) {
+  constructor ({ io, socketId, downloadsFolder, messageTimeDelay = 200 }) {
     this.io = io
     this.socketId = socketId
     this.downloadsFolder = downloadsFolder
     this.ON_UPLOAD_EVENT = 'file-upload'
+    this.messageTimeDelay = messageTimeDelay
+  }
+
+  canExecute (lastExecution) {
+    return (Date.now() - lastExecution) > this.messageTimeDelay
   }
 
   handleFileBytes (filename) {
+    this.lastMessageSent = Date.now()
     async function* handleData (source) {
       let processedAlready = 0
       for await (const chunk of source) {
         yield chunk
         processedAlready += chunk.length
+        if (!this.canExecute(this.lastMessageSent)) {
+          continue
+        }
         this.io.to(this.socketId).emit(this.ON_UPLOAD_EVENT, { processedAlready, filename })
         logger.info(`File [${filename}] got ${processedAlready} bytes to ${this.socketId}`)
       }
