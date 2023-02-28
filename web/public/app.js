b823eeab03fef1d07d406788a4bb247ff6c63a26
Gui-dev <adrianon10@gmail.com>
2023-02-28 03:39:22 -0300

    feat: add js extension

--- web/public/app.js
+++ web/public/app.js
@@ -1,1 +1,19 @@
-console.log('hello world!')
+import { AppController } from './src/app-controller.js'
+import { ConnectionManager } from './src/connection-manager.js'
+import { ViewManager } from './src/view-manager.js'
+
+const API_URL = 'https://localhost:3333'
+
+const appController = new AppController({
+  connectionManager: new ConnectionManager({
+    apiUrl: API_URL,
+  }),
+  viewManager: new ViewManager()
+})
+
+try {
+  appController.initialize()
+} catch (error) {
+  console.log('ERROR: ', error)
+}
+
